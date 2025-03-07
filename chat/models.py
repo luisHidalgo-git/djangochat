c23d42f352df17ba27b96e268348dd046b3a4099
luisHidalgo-git <hidalgovazquezluisangel2004@gmail.com>
2025-03-07 17:21:34 -0600

    mensajes enviados correcto

---
+++ chat/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+from django.conf import settings
+
+class Message(models.Model):
+    sender = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='sent_messages')
+    receiver = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='received_messages')
+    content = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)  # Asegúrate de que este campo sea aware
